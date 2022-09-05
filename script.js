a03df2835cd2c7a8bfd50fdbcb582b5b72f0ae53
malexarcos <110743192+malexarcos@users.noreply.github.com>
2022-09-05 18:43:58 -0500

    added more java code

---
+++ script.js
@@ -0,0 +1,19 @@
+function generateQuiz(questions, quizContainers, resultsContainer, submitButton){
+    function showQuestions(questions, quizContainers){
+        //code....
+    }
+    function showResults(questions, quizContainers, resultsContainer){
+        // code....
+    }
+    // enter the questions
+    showQuestions(questions, quizContainers);
+
+    // submit/show the results
+    submitButton(questions, quizContainers, resultsContainer);
+}
+
+var questions = [
+    {
+        question: "what is java"
+    }
+]
